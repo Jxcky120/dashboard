ba34cabac9bb923d0cac15a51e5d9b9aad07489e
Jxcky120 <jacky20052005@gmail.com>
2025-01-10 14:12:52 +0100

    Update timetable.tsx

--- src/app/ui/reservations/timetable.tsx
+++ src/app/ui/reservations/timetable.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import fakedata from "./fakedata";
 import clsx from "clsx";
 
@@ -10,18 +12,66 @@ const time_interval = 0.5;
 
 function time_toString(time: number) {
   const hour = Math.floor(time);
-  const minute = (time - hour) * 60; 
+  const minute = (time - hour) * 60;
   return `${hour}:${(minute < 10 ? "0" : "") + minute}`;
+}
+
+function generate_days(currDate: Date) {
+  return Array.from(
+    { length: num_days },
+    (_, i) => new Date(new Date().setDate(currDate.getDate() - 3 + i))
+  );
 }
 
 export default function timetable({ date }: { date: Date }) {
   let data = fakedata();
+  let today = new Date(date);
 
-  const today = new Date(date);
-  const list_days = [];
+  const list_days = generate_days(today);
 
-  for (let i = -3; i < num_days / 2; i++) {
-    list_days.push(new Date(new Date().setDate(today.getDate() + i)));
+  function changeDate(e: Date) {
+    today = e;
+
+    Array.from(document.querySelectorAll(".date-item")).forEach((item) => {
+      item.classList.remove("date-selected");
+
+      if (item.getAttribute("dataset-key") === e.getDate().toString()) {
+        item.classList.add("date-selected");
+      }
+    });
+
+    updateData();
+  }
+
+  function updateData() {
+    let timetable = document.querySelector(".timetable");
+    if (timetable) {
+      timetable.innerHTML = "";
+      Array.from({ length: (end_time - start_time) / time_interval + 1 }).map(
+        (_, index) =>
+          (timetable.innerHTML += `
+          <div key=${index} class="flex flex-row timetable-row">
+          <div class="time-label">
+            <p>${time_toString(start_time + index * time_interval)}</p>
+          </div>
+          <div class="timetable-cell">
+            ${data
+              .filter(
+                (item) =>
+                  item.start_time === start_time + index * time_interval &&
+                  item.date.getDay() == today.getDay()
+              )
+              .map(
+                (item) =>
+                  `<div key=${item.id} class="timetable-item">
+                <p>${item.name} (${item.people})</p>
+              </div>`
+              )
+              .join("")}
+          </div>
+        </div>`)
+      );
+    }
   }
 
   return (
@@ -32,32 +82,43 @@ export default function timetable({ date }: { date: Date }) {
         {list_days.map((day, index) => (
           <button
             key={day.getDate()}
-            className={clsx("date-item w-full", day.getDate() === today.getDate() && "date-selected")}
+            dataset-key={day.getDate()}
+            className={clsx(
+              "date-item w-full",
+              day.getDate() === today.getDate() && "date-selected"
+            )}
+            onClick={() => changeDate(day)}
           >
-            <p className="text-sm">
-                {days[day.getDay()]}
-            </p>
-            <p className="text-sm">
-                {day.getDate()}
-            </p>
+            <p className="text-sm">{days[day.getDay()]}</p>
+            <p className="text-sm">{day.getDate()}</p>
           </button>
         ))}
 
         <button className="date-right">{">"}</button>
       </div>
       <div className="flex flex-col timetable">
-        { Array.from({ length: (end_time - start_time)  / time_interval + 1 }).map((_, index) => (
+        {Array.from({
+          length: (end_time - start_time) / time_interval + 1,
+        }).map((_, index) => (
           <div key={index} className="flex flex-row timetable-row">
             <div className="time-label">
               <p>{time_toString(start_time + index * time_interval)}</p>
             </div>
             <div className="timetable-cell">
-              {data.filter((item) => item.start_time === start_time + index * time_interval).map((item) => (
-                <div key={item.id} className="timetable-item">
-                  <p>{item.name} ({item.people})</p>
-                  {/* Maybe add actions here? */}
-                </div>
-              ))}
+              {data
+                .filter(
+                  (item) =>
+                    item.start_time === start_time + index * time_interval &&
+                    item.date.getDay() == today.getDay()
+                )
+                .map((item) => (
+                  <div key={item.id} className="timetable-item">
+                    <p>
+                      {item.name} ({item.people})
+                    </p>
+                    {/* Maybe add actions here? */}
+                  </div>
+                ))}
             </div>
           </div>
         ))}
